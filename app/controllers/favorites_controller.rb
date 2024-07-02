53d62045109db6f0e8a7c5203187e6a6b0befe83
saeusu <171134641+saeusu@users.noreply.github.com>
2024-07-02 02:40:31 +0000

    asynchronous

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -4,16 +4,12 @@ class FavoritesController < ApplicationController
     book = Book.find(params[:book_id])
     favorite = current_user.favorites.new(book_id: book.id)
     favorite.save
-    redirect_to request.referer
-    # リダイレクト先要確認
   end
 
   def destroy
     book = Book.find(params[:book_id])
     favorite = current_user.favorites.find_by(book_id: book.id)
     favorite.destroy
-    redirect_to request.referer
-    # リダイレクト先要確認
   end
 
 end
